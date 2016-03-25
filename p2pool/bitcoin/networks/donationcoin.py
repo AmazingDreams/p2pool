112049db9706c2f8b0b5873abcc719f649cdb2e6
Dennis Ruhe <dennis@amazingsystems.nl>
2016-03-25 11:14:21 +0000

    Updated donationcoin settings

--- p2pool/bitcoin/networks/donationcoin.py
+++ p2pool/bitcoin/networks/donationcoin.py
@@ -8,7 +8,7 @@ from p2pool.util import pack
 
 
 P2P_PREFIX = 'fbc0b6db'.decode('hex')
-P2P_PORT = 9333
+P2P_PORT = 11060
 ADDRESS_VERSION = 48
 RPC_PORT = 21060
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
